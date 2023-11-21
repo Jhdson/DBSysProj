f8b7babca4cf3f6cbd91495f0ffe7f5235c6a10b
Lars002 <147392638+Lars002@users.noreply.github.com>
2023-11-21 18:05:39 +0800

    done Register

---
+++ ParkingSystem/Model/UserLogged.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ParkingSystem.Appdata;
+
+namespace ParkingSystem.Model
+{
+    internal class UserLogged
+    {
+
+        public Users Users { get; set; }
+
+        private UserLogged() 
+        {
+
+        }
+    }
+}
